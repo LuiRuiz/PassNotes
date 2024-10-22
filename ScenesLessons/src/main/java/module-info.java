1c6875a56e2995bd2d8f0aed41d0196a623f2178
Luis Ruiz <153479263+LuiRuiz@users.noreply.github.com>
2024-10-22 00:26:08 -0400

    Delete ScenesLessons directory

--- ScenesLessons/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.sceneslessons {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.sceneslessons to javafx.fxml;
-    exports com.example.sceneslessons;
-}
