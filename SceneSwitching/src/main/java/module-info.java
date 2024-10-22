0dcad5d69c4f9ffadcb34c185c2d451cc1d11d78
Luis Ruiz <153479263+LuiRuiz@users.noreply.github.com>
2024-10-22 00:26:00 -0400

    Delete SceneSwitching directory

--- SceneSwitching/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.sceneswitching {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.sceneswitching to javafx.fxml;
-    exports com.example.sceneswitching;
-}
