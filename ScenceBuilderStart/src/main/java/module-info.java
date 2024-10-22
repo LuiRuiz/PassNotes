77e329908912ae95558486928ba26233e491e025
Luis Ruiz <153479263+LuiRuiz@users.noreply.github.com>
2024-10-22 00:25:51 -0400

    Delete ScenceBuilderStart directory

--- ScenceBuilderStart/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.scencebuilderstart {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.scencebuilderstart to javafx.fxml;
-    exports com.example.scencebuilderstart;
-}
