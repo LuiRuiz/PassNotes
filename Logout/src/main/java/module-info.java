36013363cb29d62613c68494eb354558cad9d963
Luis Ruiz <153479263+LuiRuiz@users.noreply.github.com>
2024-10-22 00:25:06 -0400

    Delete Logout directory

--- Logout/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.logout {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.logout to javafx.fxml;
-    exports com.example.logout;
-}
